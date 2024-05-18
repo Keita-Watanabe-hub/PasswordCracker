6c69b83ce3b6d3e08d287fed9966e869cbab0ed2
Keita Watanabe <114554190+Keita-Watanabe-hub@users.noreply.github.com>
2024-05-18 23:51:45 +0300

    FileManager Class
    Save Password to file

---
+++ FileManager.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace PasswordCracker
+{
+    public static class FileManager
+    {
+        // 指定されたファイル名とコンテンツをファイルに保存する関数
+        public static void SaveToFile(string fileName, string content)
+        {
+            // 保存先のフルパスを構築
+            string filePath = Path.Combine(@"C:\Users\annon\source\repos\PasswordCracker\PasswordStore", fileName);
+
+            // ファイルに内容を書き込む
+            File.WriteAllText(filePath, content);
+        }
+    }
+}
